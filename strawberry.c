e1fcf7f0043b710f5256e9315bffbdfc0caa1898
Your Name <30612084+yoon-hyoung@users.noreply.github.com>
2018-06-20 22:56:21 +0900

    release strawberry.c

--- strawberry.c
+++ strawberry.c
@@ -4,12 +4,17 @@
 #include <signal.h>
 #include <unistd.h>
 #include <curses.h>
+#include <pthread.h>
 #define BUF 10
 #define SPACE 4
 #define ROW 10
 
 void set_up();
 
+typedef struct printval{
+	int dir;
+	int col;
+}printval;
 char *texterase;
 char *text;
 char *email;
@@ -78,13 +83,13 @@ int main()
         		}
        			else 
                 		emailID = realloc(emailID, emailsize + BUF + sizeof(char) * 6);
-            		emailsize += BUF; //expands inputsize
-     		}
-       		emailID[pos++] = c;
-    	}
-    	IDsize = pos;
-    	email = malloc(pos);
-    	strncpy(email, emailID, pos);
+                emailsize += BUF; //expands inputsize
+        }
+        emailID[pos++] = c;
+    }
+    IDsize = pos;
+    email = malloc(pos);
+    strncpy(email, emailID, pos);
 	
 	/*get time*/
 	printf("Input time (minute second)>>");
@@ -111,36 +116,66 @@ void set_up()
 void myalarm(int signum)
 {
 	int i;
-	int col = 0;
-	int dir = +1;
 	int pid = 0;
+	pthread_t t1, t2;
+	printval value;
+	void *ring();
+	void *printText(void *);
 	FILE *fp;
 
+	/*initialize value*/
+	value.dir = +1;
+	value.col = 0;
+
 	fp = popen(email, "w");
-	fprintf(fp,"Aram ringin now!\n");		
+	fprintf(fp,"Aram ringing now!");		
 	pclose(fp);
 
 	set_up();
-	while(1) {
-		move(ROW, col);
-		addstr(text);
-		refresh();
-		sleep(2);
-		move(ROW, col);
-		addstr(texterase);
-		refresh();
-		sleep(2);
-		/*chage dir when col reaches bound*/
-		if(col+(BUF*dir)+textsize > COLS-1)
-				dir = -1;
-		else if(col+(BUF*dir) < 0)
-				dir = +1;
-			
-		col += (BUF*dir);
-	}	
+
+    /*create pthread*/
+    pthread_create(&t1, NULL, ring, NULL);
+    pthread_create(&t2, NULL, (void *)printText,(void *) &value);
+    
+    /*start thread*/
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
 	
 }
 
+void *ring()
+{
+    while(1) {
+        putchar('\a');
+        putchar('\a');
+        sleep(2);
+    }
+    return NULL;
+}
 
+void *printText(void *v)
+{
+    printval val = *(printval *)v;
+    
+    while(1) {
+        move(ROW, val.col);
+        addstr(text);
+        refresh();
+        sleep(2);
+        move(ROW, val.col);
+        addstr(texterase);
+        refresh();
+        sleep(2);
+        /*chage dir when col reaches bound*/
+        if(val.col+(BUF*(val.dir))+textsize > COLS-1)
+            val.dir = -1;
+        else if(val.col+(BUF*(val.dir)) < 0)
+            val.dir = +1;
+    
+        val.col += (BUF*(val.dir));
+    }
+    
+    return NULL;
+}
 
 
